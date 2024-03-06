5f4c44fb32b54b15f300c0c036375e5aec20f502
Kingcedru <byishimocedrick@gmail.com>
2024-03-06 14:52:02 +0200

    adding routing

--- job-portal/src/App.jsx
+++ job-portal/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <nav>navbar</nav>
+      <Outlet />
     </>
   );
 }
