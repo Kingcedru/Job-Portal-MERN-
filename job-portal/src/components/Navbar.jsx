25e2543dc40836169da173d2dc714dcef8fdfc71
Kingcedru <byishimocedrick@gmail.com>
2024-03-06 16:51:37 +0200

    complete of large screens

--- job-portal/src/components/Navbar.jsx
+++ job-portal/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import { NavLink, Link } from "react-router-dom";
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const handleMenuToggler = () => {
@@ -8,14 +8,14 @@ export default function Navbar() {
 
   const navItems = [
     { path: "/", title: "Start a search" },
-    { path: "/my-job", title: "My JObs" },
+    { path: "/my-job", title: "My Jobs" },
     { path: "/salary", title: "Salary Estimate" },
     { path: "/post-job", title: "Post A Job" },
   ];
 
   return (
-    <header>
-      <nav>
+    <header className="max-w-screen-2xl container mx-auto xl:px-24 px-4">
+      <nav className="flex justify-between items-center py-6">
         <a href="/" className="flex items-center gap-2 text-2xl text-black">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -34,6 +34,27 @@ export default function Navbar() {
           </svg>
           <span>Job Portal</span>
         </a>
+        {/*nav items on large devices*/}
+        <ul className="hidden md:flex gap-12">
+          {navItems.map(({ path, title }) => (
+            <li key={path} className="text-base">
+              <NavLink
+                to={path}
+                className={({ isActive }) => (isActive ? "active" : "")}>
+                {title}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+        {/*signup and login btn*/}
+        <div className="text-base text-primary font-medium space-x-5 hidden lg:block">
+          <Link to="/login" className="py-2 px-5 border rounded">
+            Log in
+          </Link>
+          <Link to="/sign-up" className="py-2 px-5 border rounded bg-blue">
+            Sign up
+          </Link>
+        </div>
       </nav>
     </header>
   );
