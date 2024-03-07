547bb688554969c24468971d20e40672374b8bfa
Kingcedru <byishimocedrick@gmail.com>
2024-03-07 16:47:33 +0200

    getting all data

--- job-portal/src/Pages/Home.jsx
+++ job-portal/src/Pages/Home.jsx
@@ -1,11 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Banner from "../components/Banner";
 
 export default function Home() {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    fetch("jobs.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setJobs(data);
+      });
+  }, []);
+
+  // handle input changes
   const [query, setQuery] = useState("");
   const handleInputChange = (event) => {
     setQuery(event.target.value);
   };
+
+  //
+  const filteredItems = jobs.filter(
+    (job) => job.jobTitle.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  );
+  console.log(filteredItems);
   return (
     <div>
       <Banner query={query} handleInputChange={handleInputChange} />
